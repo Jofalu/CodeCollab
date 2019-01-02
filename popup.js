f31dca9af22d2e38d8c0049fdcadc61a508cf649
Sean Lin <Sean@sean.home>
2019-01-01 19:42:51 -0500

    Re-implemented Codifier slider as a switch. Added CSS for the popup.html. Added jquery.js to help with event listening.

---
+++ popup.js
@@ -0,0 +1,12 @@
+var codify = document.getElementById('form_codify').value;
+
+alert(codify)
+/*popup.js will check the value of the codifier slider in popup.html
+If the value is 1, then it will run highlight.js on the text of the Google Doc
+over an indefinite length using an event trigger.
+
+1. Implement way for popup.js to check when to codify
+2. Implement way to use highlight.js on the text of Google Docs
+3. Implement event trigger that runs highlight.js whenever the webpage saves or makes a Post Request or edits, etc.
+
+*/
